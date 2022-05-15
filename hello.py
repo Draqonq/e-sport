3591d3bc2f03bf9f6cf861e89534d9bd5154283d
Robert <smoczex13@gmail.com>
2022-05-16 01:33:41 +0200

    Test2

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-app.config['MONGO_URI'] = 'mongodb+srv://Piorob:Robpio@cluster0.cftar.mongodb.net/serwisy?retryWrites=true&w=majority'
+
 mongo = PyMongo(app)
 
 teams = mongo.db.teams
